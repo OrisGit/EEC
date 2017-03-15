87b59693ed5fd51ba465fb23ca510091a8524f50
Oris <chief.oris@gmail.com>
2017-03-15 14:08:32 +0400

    Добавил утилитный класс для перевода (пока)КРАТНЫХ единиц в систему СИ

---
+++ src/Utils/Converter.java
@@ -0,0 +1,104 @@
+package Utils;
+
+/**
+ * Created by Oris on 15.03.2017.
+ */
+public class Converter {
+    /**
+     * Переводит значение value с приставкой Экса (Э) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double ExaToSi(double value){return value*10E18;}
+
+    /**
+     * Переводит значение value с приставкой Пета (П) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double PetaToSi(double value){return value*10E15;}
+
+    /**
+     * Переводит значение value с приставкой Тера (Т) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double TeraToSi(double value){return value*10E12;}
+
+    /**
+     * Переводит значение value с приставкой Гига (Г) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double GigaToSi(double value){return value*10E9;}
+
+    /**
+     * Переводит значение value с приставкой Мега (М) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double MegaToSi(double value){return value*10E6;}
+
+    /**
+     * Переводит значение value с приставкой Кило (к) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double KiloToSi(double value){return value*10E3;}
+
+    /**
+     * Переводит значение value с приставкой Гекто (г) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double HectoToSi(double value){return value*10E2;}
+
+    /**
+     * Переводит значение value с приставкой Дека (да) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double DecaToSi(double value){return value*10E1;}
+
+    /**
+     * Переводит значение value с приставкой Деци (д) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double DeciToSi(double value){return value*10E-1;}
+
+    /**
+     * Переводит значение value с приставкой Санти (с) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double CentiToSi(double value){return value*10E-2;}
+
+    /**
+     * Переводит значение value с приставкой Милли (м) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double MilliToSi(double value){return value*10E-3;}
+
+    /**
+     * Переводит значение value с приставкой Микро (мк) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double MicroToSi(double value){return value*10E-6;}
+
+    /**
+     * Переводит значение value с приставкой Нано (н) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double NanoToSi(double value){return value*10E-9;}
+
+    /**
+     * Переводит значение value с приставкой Пико (п) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double PicoToSi(double value){return value*10E-12;}
+
+    /**
+     * Переводит значение value с приставкой Фемто (ф) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double FemtoToSi(double value){return value*10E-15;}
+
+    /**
+     * Переводит значение value с приставкой Атто (а) в систему СИ
+     * @param value - исходное значение
+     */
+    public static double AttoToSi(double value){return value*10E-18;}
+
+
+}
